df79b86a23aa3d6a64db9dd401d52f5794ad3754
ledomone <ledom.git@gmail.com>
2018-05-29 12:52:16 +0200

    Heroes and HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders} from '@angular/common/http';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    this.log(`fetched hero id=${id}`);
+    return this.http.get<Hero[]>(this.heroesUrl).find(hero => hero.id === id);
   }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
+  }
+
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+    this.log('fetched heroes');
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 }
