fda0ddf528e65a8172d9ed0a8981487af46f96ac
ledomone <ledom.git@gmail.com>
2018-05-29 21:07:28 +0200

    Delete a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,16 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class HeroService {
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
   
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, 
